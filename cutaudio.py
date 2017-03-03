fa9dd0ebfcf5b1d8e56a72083af5c6ca79c287c8
Eric Syu <ejsyu@ucsd.edu>
2017-03-03 15:47:11 -0800

    reduced audio clips to 20 sec

--- cutaudio.py
+++ cutaudio.py
@@ -3,9 +3,9 @@ from pydub import AudioSegment
 for x in range (0, 198):
 	print x
 	try:
-		song = AudioSegment.from_wav("audio-wav-60/" + str(x) + "edited.wav")
-		thirty_seconds = 30 * 1000
-		ret = song[:thirty_seconds]
+		song = AudioSegment.from_wav("audioedited-temp/" + str(x) + "edited.wav")
+		twenty_seconds = 20 * 1000
+		ret = song[:twenty_seconds]
 		ret.export("audioedited/" + str(x) + "edited.wav", format="wav")
 	except IOError:
 		print 'file not found, skipping'
